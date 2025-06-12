42e38ee221454a6f02e83bdabd501b36e84c84a6
NitroCapThrow <163688964+NitroCapThrow@users.noreply.github.com>
2025-06-12 17:08:28 +0100

    Update calc-wip.py

--- calc-wip.py
+++ calc-wip.py
@@ -4,11 +4,17 @@ type = input('''
 + for addition
 - for subtraction
 x for multiplication
-// for division\n\n
+/ for division\n\n
 ''')
 
 n1 = int(input("what is ur first number: "))
 n2 = int(input("what is ur second number: "))
 
-if type == str("+"):
+if type == "+":
     print(n1 + n2)
+if type == "-":
+    print(n1 - n2)
+if type == "x":
+    print(n1 * n2)
+if type == "/":
+    print(n1 / n2)
